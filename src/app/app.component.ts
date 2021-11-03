d9cda0afe4c8f205ee302669dc2077e4eaff8db9
Jairo_rb06 <jairorosero2@udenar.edu.co>
2021-11-03 17:45:26 -0500

    se crea proyecto007 que consume un recurso JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto007';
+  articulos:any={};
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get("https://www.datos.gov.co/resource/6he3-5m6n.json")
+      .subscribe(
+        result => {
+          this.articulos = result;
+        },
+        error => {
+          console.log('problemas');
+        }
+      );
+  }
 }
